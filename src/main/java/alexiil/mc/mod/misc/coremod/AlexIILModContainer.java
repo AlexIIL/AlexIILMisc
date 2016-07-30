6e98fd246fabdf22261202030f679d562b9b8634
AlexIIL <AlexIIL@users.noreply.github.com>
2016-07-31 00:21:41 +0100

    Update AlexIILModContainer.java

--- src/main/java/alexiil/mc/mod/misc/coremod/AlexIILModContainer.java
+++ src/main/java/alexiil/mc/mod/misc/coremod/AlexIILModContainer.java
@@ -9,8 +9,8 @@ public class AlexIILModContainer extends DummyModContainer {
     public AlexIILModContainer() {
         super(new ModMetadata());
         ModMetadata meta = getMetadata();
-        meta.modId = "AlexIILLib.CoreMod";
-        meta.name = "AlexIILLib core mod";
+        meta.modId = "alexiil_misc_core";
+        meta.name = "AlexIIL Misc core mod";
         meta.version = "";
         meta.credits = "";
         meta.authorList = Collections.singletonList("AlexIIL");
